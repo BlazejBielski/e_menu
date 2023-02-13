f38e4ff16f08addf0f23ba134977b98fd7c85d41
Blazej Bielski <bbielski@lyck.pl>
2023-02-13 16:34:52 +0100

    fix names of cards

--- backend/menucards/urls.py
+++ backend/menucards/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('dishes/', views.DishesView.as_view(), name='dishes'),
     path('dishes/<int:pk>/', views.DishItemView.as_view(), name='dish'),
     path('cards/', views.MenuCardsView.as_view(), name='cards'),
-    path('card', views.MenuCardView.as_view(), name='card'),
+    path('cards/<int:pk>', views.MenuCardView.as_view(), name='card'),
 
 ]
