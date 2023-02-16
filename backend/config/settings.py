078b7f95f0098d9267845914a4aab4ae8cf5f8af
Blazej Bielski <bbielski@lyck.pl>
2023-02-16 15:50:40 +0100

    added celery for event executions

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'drf_spectacular',
+    'celery',
 
     'users.apps.UsersConfig',
     'menucards.apps.MenucardsConfig',
