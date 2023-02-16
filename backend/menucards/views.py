90231a0a0c9a603ff1bfde8ed61a4a2b089a7721
Blazej Bielski <bbielski@lyck.pl>
2023-02-16 15:17:26 +0100

    add to do later

--- backend/menucards/views.py
+++ backend/menucards/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from django.db.models import Subquery
 
 from menucards.models import Dish, MenuCard
 from menucards.serializers import DishSerializer, MenuCardSerializer
@@ -23,7 +24,7 @@ class MenuCardsView(ListCreateAPIView):
     queryset = MenuCard.objects.all()
 
 
+# TODO add queryset for one card
 class MenuCardView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = MenuCardSerializer
-    queryset = MenuCard.objects.all()
