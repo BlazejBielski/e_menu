e01a63d57f6082c23647f93a50988f70ad27d3c1
Blazej Bielski <bbielski@lyck.pl>
2023-03-12 13:38:07 +0100

    change db schema on relations dish-cards, add picture model for images

--- backend/menucards/models.py
+++ backend/menucards/models.py
@@ -23,7 +23,15 @@ class Dish(Feature):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     preparation_time = models.DurationField()
     is_vegetarian = models.BooleanField(default=False)
-    menu_cards = models.ManyToManyField('MenuCard', related_name='%(class)s')
+    menu_cards = models.ForeignKey('MenuCard', related_name='%(class)s', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+
+class Picture(models.Model):
+    product = models.ForeignKey('Dish', on_delete=models.CASCADE, related_name='images')
+    image = models.FileField(upload_to='images/')
+
+    def __str__(self):
+        return self.image.name
